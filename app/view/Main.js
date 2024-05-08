687b03d1803c2cc0c6794394d789aca6d4994bc6
Mateo <mmurataj@optimaalbania.com>
2024-05-08 12:01:39 +0200

    first commit

---
+++ app/view/Main.js
@@ -0,0 +1,20 @@
+Ext.define("Firstapp.view.Main", {
+    extend: 'Ext.tab.Panel',
+    xtype: 'Main',
+    requires: [
+        'Ext.TitleBar',
+        'Ext.Video'
+    ],
+    config: {
+        tabBarPosition: 'bottom',
+        items: 
+        [
+		    {
+                xtype:'Home'
+            },
+		    {
+                xtype:'PostContainer'
+            }
+        ]
+    }
+});
